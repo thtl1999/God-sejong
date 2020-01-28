c75d8b2fa84d9187d299b87a91113b2222e4af06
thtl1999 <thtl2004@hanmail.net>
2020-01-28 15:34:52 +0900

    update add-jongsung

---
+++ God-sejong/addJongsung.js
@@ -0,0 +1,13 @@
+function addJongsung(word){
+    var picked_js = jongsung[Math.floor(Math.random() * jongsung.length - 1) + 1]
+    var new_word = ''
+    for(var i=0;i<word.length;i++){
+        var cjj = disassemble_hangul(word[i])
+        if (cjj[2] === '')
+            new_word += assemble_hangul([cjj[0], cjj[1], picked_js])
+        else
+            new_word += word[i]
+    }
+
+    return new_word
+}
---
+++ God-sejong/addJunk.js
@@ -0,0 +1,3 @@
+function addJunk(word){
+    
+}
